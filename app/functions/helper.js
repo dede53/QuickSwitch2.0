2b3e1f1bb7a4bf6017271a498817f048fdce98a2
dede53 <hallomarcel.hm@gmail.com>
2015-12-20 22:01:21 +0100

    json bug

--- app/functions/helper.js
+++ app/functions/helper.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var colors	= require('colors/safe');
 
 module.exports = {
@@ -39,46 +38,4 @@ module.exports = {
 				break;
 		}
 	}
-=======
-var colors	= require('colors/safe');
-
-module.exports = {
-	array_key_exists: function(key, search) {
-		if (!search || (search.constructor !== Array && search.constructor !== Object)) {
-			return false;
-		}
-		return key in search;
-	},
-	inArray: function(needle, haystack) {
-		var length = haystack.length;
-		for(var i = 0; i < length; i++) {
-			if(haystack[i] == needle) return true;
-		}
-		return false;
-	},
-	log: function(msg, type){
-		var now = new Date;
-		var datum =  now.getDate() + ":" + (now.getMonth() + 1) + ":" + now.getFullYear() + " " + now.getHours() + ":" + now.getMinutes() + ":" + now.getSeconds();
-		switch(type){
-			case "info":
-				console.log(datum +': '+ colors.green(msg));
-				break;
-			case "data":
-				console.log(datum +': '+ colors.grey(msg));
-				break;
-			case "help":
-				console.log(datum +': '+ colors.blue(msg));
-				break;
-			case "debug":
-				console.log(datum +': '+ colors.blue(msg));
-				break;
-			case "warn":
-				console.log(datum +': '+ colors.yellow(msg));
-				break;
-			case "error":
-				console.log(datum +': '+ colors.red(msg));
-				break;
-		}
-	}
->>>>>>> af97e501eae31491992417dd0f792413c9d64b8f
 }
--- app/functions/onewire.js
+++ app/functions/onewire.js
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-
 var conf 			= require('./../../config.json');
 var w1bus			= require('node-w1bus');
 var request 		= require('request');
@@ -45,52 +43,4 @@ module.exports = {
 			}
 		});
 	}
-=======
-
-var conf 			= require('./../../config.json');
-var w1bus			= require('node-w1bus');
-var request 		= require('request');
-var bus				= w1bus.create();
-var config			= bus.getConfig();
-
-module.exports = {
-	saveSensors : function(){
-		console.log("Lese Temperaturen...");
-		bus.listAllSensors().then(function(data){
-			if(data.err){
-				console.log(data);
-			}else{
-				var opt_measureType = "temperature";
-				data.ids.forEach(function(sensor){
-					bus.getValueFrom(sensor, opt_measureType).then(function(res){
-						if(res.err){
-							console.log(res);
-						}else{
-							res.result.id = sensor;
-							res.result.value = Math.round(res.result.value * 100);
-							
-							// Ausgelesene Daten an die Datenbank schicken
-							request.post({
-								url:'http://' + conf.QuickSwitch.ip + ':' + conf.QuickSwitch.port + '/newdata/',
-								form:
-									{
-										nodeID : res.result.id,
-										supplyV : 2.2,
-										temp : res.result.value,
-										timestamp : res.result.timestamp
-									}
-							},function( err, httpResponse, body){
-								if(err){
-									console.log( err );
-								}else{
-									console.log( res.result.id + " gespeichert!");
-								}
-							});
-						}
-					});	
-				});
-			}
-		});
-	}
->>>>>>> af97e501eae31491992417dd0f792413c9d64b8f
 }
