ca376a18b29082e1647edb239aaa26b16a23f633
dede53 <hallomarcel.hm@gmail.com>
2016-08-14 02:47:01 +0200

    new timer visu

--- public/mobile/app/timer/controller.js
+++ public/mobile/app/timer/controller.js
@@ -9,9 +9,12 @@ app.controller('timerController',function($scope, $rootScope, socket){
 		"6":"Samstag"
 	}
 	$scope.deleteTimer = function(data) {
-		delete $scope.timers[data.name];
-		socket.emit('deleteTimer', {"id":data.id});	
+		socket.emit('deleteTimer', data);
 	}
+	$scope.testActions = function(data) {
+		socket.emit('testActions', data);
+	}
+
 	$scope.switchTimer = function(data) {
 		if(data.active == "true"){
 			data.active = "false";
@@ -19,51 +22,31 @@ app.controller('timerController',function($scope, $rootScope, socket){
 			data.active = "true";
 		}
 		socket.emit('switchTimer', data);
-	    if ($scope.timers[data.name].clickIcon === 'notifications') {
-	        $scope.timers[data.name].clickIcon = 'notifications_off';
-	        $scope.timers[data.name].fill = '#ffc0c0';
+	    if ($rootScope.timers[data.id].clickIcon === 'notifications') {
+	        $rootScope.timers[data.id].clickIcon = 'notifications_off';
+	        $rootScope.timers[data.id].fill = '#ffc0c0';
 	    }
 	    else {
-	        $scope.timers[data.name].clickIcon = 'notifications';
-	        $scope.timers[data.name].fill = 'lightgreen';
+	        $rootScope.timers[data.id].clickIcon = 'notifications';
+	        $rootScope.timers[data.id].fill = 'lightgreen';
 	    }
 	};
 
 	socket.emit('timers');
-	$scope.timers = {};
-	socket.on('timer', function(data){
-		if($rootScope.activeUser.name == data.user){
-			if(data.active == "true"){
-				data.clickIcon = "notifications";
-				data.fill = 'lightgreen';
-			}else{
-				data.clickIcon = "notifications_off";
-		        data.fill = '#ffc0c0';
-			}
-			$scope.timers[data.name] = data;
-		}else{
-			delete $scope.timers[data.name];
-		}
+	$rootScope.timers = {};
+	socket.on('deleteTimer', function(id){
+		delete $rootScope.timers[id];
+		// socket.emit('timers');
+		// $scope.timers = {};
 	});
-});
-
-app.controller('newTimer', function($scope, socket) {
-	
-});
-app.controller('createTimerController', function($scope, socket) {
-	/*
-	$scope.size = 48;
-
-	$scope.clickIcon = 'notifications';
-	$scope.clickIconMorph = function(name) {
-	    if ($scope.timers[name].clickIcon === 'notifications') {
-	        $scope.timers[name].clickIcon = 'notifications_off';
-	        $scope.timers[name].fill = 'pink';
-	    }
-	    else {
-	        $scope.timers[name].clickIcon = 'notifications';
-	        $scope.timers[name].fill = 'lightgreen';
-	    }
-	};
-	*/
+	socket.on('timer', function(data){
+		if(data.active == "true"){
+			data.clickIcon = "notifications";
+			data.fill = 'lightgreen';
+		}else{
+			data.clickIcon = "notifications_off";
+			data.fill = '#ffc0c0';
+		}
+		$rootScope.timers[data.id] = data;
+	});
 });
