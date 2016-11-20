2fe40d2ab6e250a48ae98c4507a125c49fee28fc
dede53 <hallomarcel.hm@gmail.com>
2016-11-20 12:03:10 +0100

    solve probem with eventlistener

--- app/functions/database.js
+++ app/functions/database.js
@@ -1,8 +1,13 @@
 var mysql			=	require('mysql');
 var conf 			=	require('./../../config.json');
+<<<<<<< HEAD
+var helper 			=	require('./helper.js');
+var pool      		=	mysql.createPool({
+=======
 var helper 			= require('./helper.js');
 
 var pool      =    mysql.createPool({
+>>>>>>> d3e70a1d720f830c1b7fd87dccb9dd8e639e7874
 	connectionLimit : 100,
 	host     : conf.mysql.host,
 	user     : conf.mysql.user,
@@ -11,6 +16,8 @@ var pool      =    mysql.createPool({
 	debug    :  false
 });
 
+<<<<<<< HEAD
+=======
 
 /*
 require('events').EventEmitter.prototype._maxListeners = 100;
@@ -28,10 +35,42 @@ client.on("error", function (err) {
 });
 module.exports = client;
 */
+>>>>>>> d3e70a1d720f830c1b7fd87dccb9dd8e639e7874
 module.exports = {
 	//client: client,
 	all : function(query, callback) {
 		pool.getConnection(function(err,connection){
+<<<<<<< HEAD
+			if(err){
+				console.log(err);
+				connection.release();
+				callback(err, null);
+				return;
+			}
+			connection.query(query, function(err,rows){
+				connection.release();
+				if(err) {
+					console.log(err);
+					callback(err, rows);
+					return;
+				}
+				callback(err, rows);
+			});				
+			
+			// if (err) {
+			// 	callback(err);
+			// }else{
+			// 	connection.query(query, function(err,rows){
+			// 		if(err) {
+			// 			helper.log.error({"code" : 100, "status" : "Error in connection database"});
+			// 			helper.log.error(err);
+			// 		}else{
+			// 			callback(err, rows);
+			// 			connection.release();
+			// 		}
+			// 	});
+			// }
+=======
 			// helper.log.debug(query);
 			// helper.log.debug('connected as id ' + connection.threadId);
 			if (err) {
@@ -60,20 +99,41 @@ module.exports = {
 				});
 */
 			}
+>>>>>>> d3e70a1d720f830c1b7fd87dccb9dd8e639e7874
 		});
 	},
 	run: function(query){
 		pool.getConnection(function(err,connection){
+<<<<<<< HEAD
+			if (err) {
+				console.log(err);
+				connection.release();
+=======
 			// helper.log.debug(query);
 			// helper.log.debug('connected as id ' + connection.threadId);
 			if (err) {
 				connection.release();
 				helper.log.error({"code" : 100, "status" : "Error in connection database"});
+>>>>>>> d3e70a1d720f830c1b7fd87dccb9dd8e639e7874
 				return;
 			}
 
 
 			connection.query(query, function(err,rows){
+<<<<<<< HEAD
+				connection.release();
+				if(err){
+					console.log(err);
+					return;
+				}
+			});
+
+			// connection.on('error', function(err) {
+			// 	console.log(err);
+			// 	return;
+			// });
+
+=======
 				if(err){
 					helper.log.error(err);
 					connection.release();
@@ -88,21 +148,50 @@ module.exports = {
 				return;
 			});
 */
+>>>>>>> d3e70a1d720f830c1b7fd87dccb9dd8e639e7874
 		});
 	},
 	each : function(query, callback) {
 		pool.getConnection(function(err,connection){
 			helper.log.debug('connected as id ' + connection.threadId);
 			if (err) {
+<<<<<<< HEAD
+				console.log(err);
+				callback(err);
+				return;
+=======
 				connection.release();
 				helper.log.error({"code" : 100, "status" : "Error in connection database"});
 				callback(err);
 				// return;
+>>>>>>> d3e70a1d720f830c1b7fd87dccb9dd8e639e7874
 			}
 
 
 			connection.query(query, function(err,rows){
 				connection.release();
+<<<<<<< HEAD
+				
+				if(err){
+					console.log(err);
+					return;
+				}
+				connection.removeEventListener('error', errorHandler);
+				rows.forEach(function(row){
+					callback(err, row);
+				});
+			});
+
+			connection.on('error', errorHandler);
+		});
+	}
+}
+
+function errorHandler(err){
+	helper.log.error({"code" : 100, "status" : "Error in connection database"});
+	callback(err);
+	return;
+=======
 				if(!err) {
 					rows.forEach(function(row){
 						callback(err, row);
@@ -119,4 +208,5 @@ module.exports = {
 			});
 		});
 	}
+>>>>>>> d3e70a1d720f830c1b7fd87dccb9dd8e639e7874
 }
