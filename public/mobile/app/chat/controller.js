60a10fb1954eaac79033a6c935248bc441497b63
dede53 <hallomarcel.hm@gmail.com>
2017-01-10 21:41:28 +0100

    make the chat work again with new structure

--- public/mobile/app/chat/controller.js
+++ public/mobile/app/chat/controller.js
@@ -1,45 +1,18 @@
-app.controller("chatController", function($scope, socket, $location, $anchorScroll){
-
-	$scope.moreMessagesAvible = true;
-	$scope.sharedMessages = new Array;
-	$scope.link = {};
-	$scope.link.type = "1";
-
-	if($scope.sharedMessages.length == "0"){
-		var now = Math.floor(Date.parse(new Date));
-		socket.emit('loadOldMessages', now );
+app.controller("chatController", function($rootScope, $scope, socket){
+	$scope.link = {
+		type:1,
+		message:""
+	}
+	if(Object.keys($rootScope.chatMessages).length <= 0){
+		socket.emit("messages:loadOld", new Date().getTime());
 	}
 
-	socket.on('linkMessage', function(data){
-		if(data.moreMessagesAvible == true){
-			$scope.moreMessagesAvible = true;
-			data.messages.forEach(function(message){
-				$scope.sharedMessages.splice(0, 0, message);
-			});
-		}else{
-			$scope.moreMessagesAvible = false;
-		}
-	});
-	socket.on('newLinkMessage', function(data){
-		$scope.sharedMessages.push(data);
-	});
-	
 	$scope.loadOldMessages = function(){
-		socket.emit('loadOldMessages', $scope.sharedMessages[0].time);
+		socket.emit('messages:loadOld', $rootScope.chatMessages[Object.keys($rootScope.chatMessages)[0]].time);
 	}
-});
-
-app.controller('sendNewMessage', function($scope, socket) {
-	$scope.sendMessage = function() {
-		// Validierung!!
-		$scope.linkMessage = {
-			author: $scope.activeUser.name,
-			message: $scope.link.message,
-			type: $scope.link.type
-		}
+	$scope.sendMessage = function(link) {
+		socket.emit('messages:add', {user:$rootScope.activeUser, add: link});
 		$scope.link.message = "";
 		$scope.link.type = "1";
-		socket.emit('newLinkMessage', $scope.linkMessage);
-		
 	};
 });
