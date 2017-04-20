5686f29b53fa10e26a9758f3a3423b9402a6cadf
dede53 <hallomarcel.hm@gmail.com>
2017-04-20 12:16:12 +0200

    add 'not saved yet' message for settings

--- public/settings/app/home/controller.js
+++ public/settings/app/home/controller.js
@@ -1,4 +1,5 @@
 app.controller('homeController', function($scope, socket, $timeout){
+	$scope.changed = false;
 	socket.emit('settings:get');
 	$scope.addSwitchserver = function(){
 		$scope.settings.switchserver.push({
@@ -7,12 +8,14 @@ app.controller('homeController', function($scope, socket, $timeout){
 			"port": 4040,
 			"ip": "192.168.187.123"
 		});
+		$scope.changed = true;
 	};
 	$scope.removeSwitchserver = function(id){
+		$scope.changed = true;
 		$scope.settings.switchserver.splice(id, 1);
 	};
 	$scope.save = function(data){
-		console.log(data);
+		$scope.changed = false;
 		socket.emit('settings:save', data);
 	}
 });
