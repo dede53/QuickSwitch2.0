f40e39a6b7e162f1a055df2718f826c57c642358
dede53 <hallomarcel.hm@gmail.com>
2017-07-05 23:43:46 +0200

    fix a lot of issues: shart group devices for example..

--- SmartHome.sql
+++ SmartHome.sql
@@ -3,20 +3,14 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Erstellungszeit: 05. Apr 2017 um 13:27
--- Server-Version: 5.5.54-0+deb8u1
--- PHP-Version: 5.6.29-0+deb8u1
+-- Erstellungszeit: 05. Jul 2017 um 23:42
+-- Server-Version: 5.5.55-0+deb8u1
+-- PHP-Version: 5.6.30-0+deb8u1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Datenbank: `SmartHome`
@@ -35,6 +29,10 @@ CREATE TABLE `charttypen` (
   `chart` varchar(20) NOT NULL,
   `name` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- RELATIONEN DER TABELLE `charttypen`:
+--
 
 --
 -- Daten für Tabelle `charttypen`
@@ -65,6 +63,10 @@ CREATE TABLE `countdowns` (
   `user` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- RELATIONEN DER TABELLE `countdowns`:
+--
+
 -- --------------------------------------------------------
 
 --
@@ -75,6 +77,10 @@ CREATE TABLE `countdowntypen` (
   `id` int(11) NOT NULL,
   `type` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- RELATIONEN DER TABELLE `countdowntypen`:
+--
 
 --
 -- Daten für Tabelle `countdowntypen`
@@ -94,7 +100,7 @@ INSERT INTO `countdowntypen` (`id`, `type`) VALUES
 CREATE TABLE `devices` (
   `deviceid` int(11) NOT NULL,
   `status` int(50) NOT NULL,
-  `showStatus` boolean(20) NOT NULL DEFAULT 1,
+  `showStatus` tinyint(1) NOT NULL DEFAULT '1',
   `name` varchar(40) NOT NULL,
   `protocol` varchar(40) NOT NULL,
   `buttonLabelOn` varchar(20) NOT NULL,
@@ -106,6 +112,10 @@ CREATE TABLE `devices` (
   `type` varchar(30) NOT NULL DEFAULT 'device'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- RELATIONEN DER TABELLE `devices`:
+--
+
 -- --------------------------------------------------------
 
 --
@@ -115,9 +125,13 @@ CREATE TABLE `devices` (
 CREATE TABLE `groups` (
   `id` int(11) NOT NULL,
   `name` varchar(20) NOT NULL,
-  `devices` varchar(200) NOT NULL,
+  `devices` varchar(20000) NOT NULL,
   `user` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- RELATIONEN DER TABELLE `groups`:
+--
 
 -- --------------------------------------------------------
 
@@ -130,6 +144,10 @@ CREATE TABLE `linetypen` (
   `name` varchar(30) NOT NULL,
   `line` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- RELATIONEN DER TABELLE `linetypen`:
+--
 
 --
 -- Daten für Tabelle `linetypen`
@@ -162,6 +180,10 @@ CREATE TABLE `messages` (
   `message` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- RELATIONEN DER TABELLE `messages`:
+--
+
 -- --------------------------------------------------------
 
 --
@@ -174,12 +196,32 @@ CREATE TABLE `messagetypen` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
+-- RELATIONEN DER TABELLE `messagetypen`:
+--
+
+--
 -- Daten für Tabelle `messagetypen`
 --
 
 INSERT INTO `messagetypen` (`id`, `type`) VALUES
 (1, 'Text'),
 (2, 'Link');
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `objects`
+--
+
+CREATE TABLE `objects` (
+  `id` int(11) NOT NULL,
+  `type` varchar(20) NOT NULL,
+  `data` varchar(1000) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- RELATIONEN DER TABELLE `objects`:
+--
 
 -- --------------------------------------------------------
 
@@ -191,6 +233,10 @@ CREATE TABLE `rooms` (
   `id` int(11) NOT NULL,
   `name` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- RELATIONEN DER TABELLE `rooms`:
+--
 
 -- --------------------------------------------------------
 
@@ -207,6 +253,10 @@ CREATE TABLE `sensors` (
   `linecolor` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- RELATIONEN DER TABELLE `sensors`:
+--
+
 -- --------------------------------------------------------
 
 --
@@ -214,11 +264,33 @@ CREATE TABLE `sensors` (
 --
 
 CREATE TABLE `stored_vars` (
-  -- `id` int(11) NOT NULL,
-  `id` varchar(20) NOT NULL,
+  `uid` int(11) NOT NULL,
+  `id` varchar(100) NOT NULL,
   `time` varchar(20) NOT NULL,
   `value` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- RELATIONEN DER TABELLE `stored_vars`:
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabellenstruktur für Tabelle `switch_history`
+--
+
+CREATE TABLE `switch_history` (
+  `id` int(11) NOT NULL,
+  `deviceid` int(20) NOT NULL,
+  `time` varchar(20) NOT NULL,
+  `status` varchar(20) NOT NULL,
+  `place` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- RELATIONEN DER TABELLE `switch_history`:
+--
 
 -- --------------------------------------------------------
 
@@ -234,8 +306,12 @@ CREATE TABLE `timer` (
   `conditions` varchar(500) NOT NULL,
   `actions` text NOT NULL,
   `user` varchar(20) NOT NULL DEFAULT 'system',
-  `lastexec` varchar(15) NOT NULL
+  `lastexec` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- RELATIONEN DER TABELLE `timer`:
+--
 
 -- --------------------------------------------------------
 
@@ -249,24 +325,25 @@ CREATE TABLE `user` (
   `favoritDevices` varchar(200) NOT NULL DEFAULT '[]',
   `favoritVariables` varchar(200) NOT NULL DEFAULT '[]',
   `varChart` varchar(200) NOT NULL DEFAULT '[]',
-  `variables` varchar(200) NOT NULL DEFAULT '[]',
-  `admin` varchar(20) NOT NULL DEFAULT 'false',
+  `admin` tinyint(1) NOT NULL DEFAULT '0',
   `chartHour` int(20) NOT NULL DEFAULT '24',
   `background` varchar(500) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- RELATIONEN DER TABELLE `user`:
+--
+
 -- --------------------------------------------------------
-
-INSERT INTO `user` (`id`, `name`, `favoritDevices`, `favoritVariables`, `varChart`, `variables`, `admin`, `background`) VALUES (1, 'Admin', '[]', '[]','[]', '[]', 'true', '');
-
 
 --
 -- Tabellenstruktur für Tabelle `variable`
 --
 
 CREATE TABLE `variable` (
-  `uid` int(11) NOT NULL AUTO_INCREMENT,
+  `uid` int(11) NOT NULL,
   `id` varchar(255) NOT NULL,
+  `nodeid` varchar(255) NOT NULL,
   `name` varchar(255) NOT NULL,
   `status` varchar(20) NOT NULL,
   `charttype` varchar(255) NOT NULL,
@@ -277,8 +354,15 @@ CREATE TABLE `variable` (
   `step` varchar(20) NOT NULL DEFAULT 'false',
   `showall` varchar(20) NOT NULL DEFAULT 'false',
   `user` varchar(20) NOT NULL DEFAULT 'system',
-  `lastChange` varchar(20) NOT NULL
+  `lastChange` varchar(20) NOT NULL,
+  `saveActive` varchar(20) NOT NULL DEFAULT 'false',
+  `saveType` varchar(20) NOT NULL DEFAULT 'onchange',
+  `saveInterval` int(10) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- RELATIONEN DER TABELLE `variable`:
+--
 
 --
 -- Indizes der exportierten Tabellen
@@ -339,6 +423,12 @@ ALTER TABLE `messagetypen`
   ADD UNIQUE KEY `id` (`id`);
 
 --
+-- Indizes für die Tabelle `objects`
+--
+ALTER TABLE `objects`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indizes für die Tabelle `rooms`
 --
 ALTER TABLE `rooms`
@@ -357,8 +447,13 @@ ALTER TABLE `sensors`
 -- Indizes für die Tabelle `stored_vars`
 --
 ALTER TABLE `stored_vars`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `id` (`id`);
+  ADD PRIMARY KEY (`uid`);
+
+--
+-- Indizes für die Tabelle `switch_history`
+--
+ALTER TABLE `switch_history`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indizes für die Tabelle `timer`
@@ -378,9 +473,8 @@ ALTER TABLE `user`
 -- Indizes für die Tabelle `variable`
 --
 ALTER TABLE `variable`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `id` (`id`),
-  ADD UNIQUE KEY `name` (`name`);
+  ADD PRIMARY KEY (`uid`),
+  ADD UNIQUE KEY `uid` (`uid`);
 
 --
 -- AUTO_INCREMENT für exportierte Tabellen
@@ -405,12 +499,12 @@ ALTER TABLE `countdowntypen`
 -- AUTO_INCREMENT für Tabelle `devices`
 --
 ALTER TABLE `devices`
-  MODIFY `deviceid` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `deviceid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 --
 -- AUTO_INCREMENT für Tabelle `groups`
 --
 ALTER TABLE `groups`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT für Tabelle `linetypen`
 --
@@ -420,17 +514,22 @@ ALTER TABLE `linetypen`
 -- AUTO_INCREMENT für Tabelle `messages`
 --
 ALTER TABLE `messages`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 --
 -- AUTO_INCREMENT für Tabelle `messagetypen`
 --
 ALTER TABLE `messagetypen`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
+-- AUTO_INCREMENT für Tabelle `objects`
+--
+ALTER TABLE `objects`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+--
 -- AUTO_INCREMENT für Tabelle `rooms`
 --
 ALTER TABLE `rooms`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 --
 -- AUTO_INCREMENT für Tabelle `sensors`
 --
@@ -440,23 +539,24 @@ ALTER TABLE `sensors`
 -- AUTO_INCREMENT für Tabelle `stored_vars`
 --
 ALTER TABLE `stored_vars`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40846;
+--
+-- AUTO_INCREMENT für Tabelle `switch_history`
+--
+ALTER TABLE `switch_history`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4633;
 --
 -- AUTO_INCREMENT für Tabelle `timer`
 --
 ALTER TABLE `timer`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 --
 -- AUTO_INCREMENT für Tabelle `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT für Tabelle `variable`
 --
 ALTER TABLE `variable`
-  MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT;COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+  MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;COMMIT;
