264bc0b4181bf001fcb0ae6b498712391b883992
Daniel <hallomarcel.hm@gmail.com>
2017-10-10 23:44:43 -0300

    improve

--- SmartHome.sql
+++ SmartHome.sql
@@ -26,8 +26,8 @@ USE `SmartHome`;
 
 CREATE TABLE IF NOT EXISTS `charttypen` (
   `id` bigint(11) NOT NULL AUTO_INCREMENT,
-  `chart` varchar(20) NOT NULL,
-  `name` varchar(20) NOT NULL,
+  `chart` varchar(20) NOT NULL DEFAULT '',
+  `name` varchar(20) NOT NULL DEFAULT '',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 
@@ -39,11 +39,11 @@ CREATE TABLE IF NOT EXISTS `charttypen` (
 
 CREATE TABLE IF NOT EXISTS `countdowns` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `type` int(11) NOT NULL,
-  `time` varchar(20) NOT NULL,
-  `switchid` int(11) NOT NULL,
-  `status` int(11) NOT NULL,
-  `user` varchar(20) NOT NULL,
+  `type` int(11) NOT NULL DEFAULT 0,
+  `time` varchar(20) NOT NULL DEFAULT '',
+  `switchid` int(11) NOT NULL DEFAULT 0,
+  `status` int(11) NOT NULL DEFAULT 0,
+  `user` varchar(20) NOT NULL DEFAULT '',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
 
@@ -55,7 +55,7 @@ CREATE TABLE IF NOT EXISTS `countdowns` (
 
 CREATE TABLE IF NOT EXISTS `countdowntypen` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `type` varchar(20) NOT NULL,
+  `type` varchar(20) NOT NULL DEFAULT '',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
@@ -67,16 +67,16 @@ CREATE TABLE IF NOT EXISTS `countdowntypen` (
 
 CREATE TABLE IF NOT EXISTS `devices` (
   `deviceid` int(11) NOT NULL AUTO_INCREMENT,
-  `status` int(50) NOT NULL,
+  `status` int(50) NOT NULL DEFAULT '1',
   `showStatus` int(2) NOT NULL DEFAULT '1',
-  `name` varchar(40) NOT NULL,
-  `protocol` varchar(40) NOT NULL,
-  `buttonLabelOn` varchar(20) NOT NULL,
-  `buttonLabelOff` varchar(20) NOT NULL,
-  `CodeOn` varchar(200) NOT NULL,
-  `CodeOff` varchar(200) NOT NULL,
-  `roomid` int(11) NOT NULL,
-  `switchserver` int(11) NOT NULL,
+  `name` varchar(40) NOT NULL DEFAULT '',
+  `protocol` varchar(40) NOT NULL DEFAULT '',
+  `buttonLabelOn` varchar(20) NOT NULL DEFAULT '',
+  `buttonLabelOff` varchar(20) NOT NULL DEFAULT '',
+  `CodeOn` varchar(200) NOT NULL DEFAULT '',
+  `CodeOff` varchar(200) NOT NULL DEFAULT '',
+  `roomid` int(11) NOT NULL DEFAULT 1,
+  `switchserver` int(11) NOT NULL DEFAULT 1,
   `type` varchar(30) NOT NULL DEFAULT 'device',
   PRIMARY KEY (`deviceid`)
 ) ENGINE=InnoDB AUTO_INCREMENT=43 DEFAULT CHARSET=latin1;
@@ -89,9 +89,9 @@ CREATE TABLE IF NOT EXISTS `devices` (
 
 CREATE TABLE IF NOT EXISTS `groups` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(20) NOT NULL,
-  `devices` varchar(200) NOT NULL,
-  `user` varchar(20) NOT NULL,
+  `name` varchar(20) NOT NULL DEFAULT '',
+  `devices` varchar(200) NOT NULL DEFAULT '',
+  `user` varchar(20) NOT NULL DEFAULT '',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 
@@ -103,8 +103,8 @@ CREATE TABLE IF NOT EXISTS `groups` (
 
 CREATE TABLE IF NOT EXISTS `linetypen` (
   `id` bigint(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(30) NOT NULL,
-  `line` varchar(20) NOT NULL,
+  `name` varchar(30) NOT NULL DEFAULT '',
+  `line` varchar(20) NOT NULL DEFAULT '',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
 
@@ -116,10 +116,10 @@ CREATE TABLE IF NOT EXISTS `linetypen` (
 
 CREATE TABLE IF NOT EXISTS `messages` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `time` varchar(20) NOT NULL,
-  `type` int(11) NOT NULL,
-  `author` varchar(20) NOT NULL,
-  `message` varchar(200) NOT NULL,
+  `time` varchar(20) NOT NULL DEFAULT '',
+  `type` int(11) NOT NULL DEFAULT 1,
+  `author` varchar(20) NOT NULL DEFAULT '',
+  `message` varchar(200) NOT NULL DEFAULT '',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=latin1;
 
@@ -131,7 +131,7 @@ CREATE TABLE IF NOT EXISTS `messages` (
 
 CREATE TABLE IF NOT EXISTS `messagetypen` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `type` varchar(11) NOT NULL,
+  `type` varchar(11) NOT NULL DEFAULT '',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
@@ -143,7 +143,7 @@ CREATE TABLE IF NOT EXISTS `messagetypen` (
 
 CREATE TABLE IF NOT EXISTS `rooms` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(30) NOT NULL,
+  `name` varchar(30) NOT NULL DEFAULT '',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
 
@@ -153,14 +153,14 @@ CREATE TABLE IF NOT EXISTS `rooms` (
 -- Tabellenstruktur für Tabelle `sensors`
 --
 
-CREATE TABLE IF NOT EXISTS `sensors` (
-  `id` bigint(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `nodeid` varchar(255) NOT NULL,
-  `charttype` varchar(255) NOT NULL,
-  `linetype` varchar(255) NOT NULL,
-  `linecolor` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+-- CREATE TABLE IF NOT EXISTS `sensors` (
+--   `id` bigint(11) NOT NULL DEFAULT '',
+--   `name` varchar(255) NOT NULL DEFAULT '',
+--   `nodeid` varchar(255) NOT NULL DEFAULT '',
+--   `charttype` varchar(255) NOT NULL DEFAULT '',
+--   `linetype` varchar(255) NOT NULL DEFAULT '',
+--   `linecolor` varchar(255) NOT NULL DEFAULT ''
+-- ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -170,9 +170,9 @@ CREATE TABLE IF NOT EXISTS `sensors` (
 
 CREATE TABLE IF NOT EXISTS `stored_vars` (
   `uid` int(11) NOT NULL AUTO_INCREMENT,
-  `id` varchar(255) NOT NULL,
-  `time` varchar(20) NOT NULL,
-  `value` varchar(20) NOT NULL,
+  `id` varchar(255) NOT NULL DEFAULT '',
+  `time` varchar(20) NOT NULL DEFAULT '',
+  `value` varchar(20) NOT NULL DEFAULT '',
   PRIMARY KEY (`uid`),
   UNIQUE KEY `id` (`uid`)
 ) ENGINE=InnoDB AUTO_INCREMENT=57149 DEFAULT CHARSET=latin1;
@@ -184,10 +184,10 @@ CREATE TABLE IF NOT EXISTS `stored_vars` (
 --
 
 CREATE TABLE IF NOT EXISTS `switch_history` (
-  `deviceid` int(20) NOT NULL,
-  `time` int(20) NOT NULL,
-  `status` int(10) NOT NULL,
-  `place` varchar(300) NOT NULL
+  `deviceid` int(20) NOT NULL DEFAULT 0,
+  `time` varchar(20) NOT NULL DEFAULT 0,
+  `status` int(10) NOT NULL DEFAULT 1,
+  `place` varchar(300) NOT NULL DEFAULT ''
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -198,13 +198,13 @@ CREATE TABLE IF NOT EXISTS `switch_history` (
 
 CREATE TABLE IF NOT EXISTS `timer` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) NOT NULL,
+  `name` varchar(100) NOT NULL DEFAULT '',
   `active` varchar(10) NOT NULL DEFAULT 'true',
-  `variables` varchar(1000) NOT NULL,
-  `conditions` varchar(500) NOT NULL,
-  `actions` text NOT NULL,
+  `variables` varchar(1000) NOT NULL DEFAULT '',
+  `conditions` varchar(1000) NOT NULL DEFAULT '',
+  `actions` varchar(1000) NOT NULL DEFAULT '',
   `user` varchar(20) NOT NULL DEFAULT 'system',
-  `lastexec` varchar(15) NOT NULL,
+  `lastexec` varchar(15) NOT NULL DEFAULT '',
   PRIMARY KEY (`id`),
   UNIQUE KEY `id` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=latin1;
@@ -217,14 +217,14 @@ CREATE TABLE IF NOT EXISTS `timer` (
 
 CREATE TABLE IF NOT EXISTS `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(20) NOT NULL,
+  `name` varchar(20) NOT NULL DEFAULT '',
   `favoritDevices` varchar(200) NOT NULL DEFAULT '[]',
   `favoritVariables` varchar(200) NOT NULL DEFAULT '[]',
   `varChart` varchar(200) NOT NULL DEFAULT '[]',
   `variables` varchar(200) NOT NULL DEFAULT '[]',
   `admin` varchar(20) NOT NULL DEFAULT 'false',
-  `chartHour` int(20) NOT NULL DEFAULT '24',
-  `background` varchar(500) NOT NULL,
+  `chartHour` int(20) NOT NULL DEFAULT 24,
+  `background` varchar(500) NOT NULL DEFAULT '',
   PRIMARY KEY (`id`),
   UNIQUE KEY `id` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
@@ -237,21 +237,21 @@ CREATE TABLE IF NOT EXISTS `user` (
 
 CREATE TABLE IF NOT EXISTS `variable` (
   `uid` int(11) NOT NULL AUTO_INCREMENT,
-  `id` varchar(255) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `status` varchar(20) NOT NULL,
-  `charttype` varchar(255) NOT NULL,
-  `linetype` varchar(255) NOT NULL,
-  `linecolor` varchar(255) NOT NULL,
-  `suffix` varchar(10) NOT NULL,
-  `error` varchar(255) NOT NULL,
+  `id` varchar(255) NOT NULL DEFAULT '',
+  `name` varchar(255) NOT NULL DEFAULT '',
+  `status` varchar(20) NOT NULL DEFAULT '',
+  `charttype` varchar(255) NOT NULL DEFAULT '',
+  `linetype` varchar(255) NOT NULL DEFAULT '',
+  `linecolor` varchar(255) NOT NULL DEFAULT '',
+  `suffix` varchar(10) NOT NULL DEFAULT '',
+  `error` varchar(255) NOT NULL DEFAULT '',
   `step` varchar(20) NOT NULL DEFAULT 'false',
   `showall` varchar(20) NOT NULL DEFAULT 'false',
   `user` varchar(20) NOT NULL DEFAULT 'system',
   `saveActive` varchar(20) NOT NULL DEFAULT 'false',
   `saveType` varchar(20) NOT NULL DEFAULT 'onchange',
   `saveInterval` int(11) NOT NULL DEFAULT '5',
-  `lastChange` varchar(20) NOT NULL,
+  `lastChange` varchar(20) NOT NULL DEFAULT '',
   PRIMARY KEY (`uid`),
   UNIQUE KEY `uid` (`uid`)
 ) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=latin1;
